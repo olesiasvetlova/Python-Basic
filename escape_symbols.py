6347a6cbe598b2a2e32f53aa56f3c20c4a4a84e0
olesiasvetlova <lesiapaska91@gmail.com>
2022-12-07 16:49:44 +0200

    add homework

---
+++ escape_symbols.py
@@ -0,0 +1,25 @@
+
+#task 3
+print('\timport antigravity')
+print('\timport __hello__')
+print('\tfrom __future__ import braces')
+print('\n')
+
+#task 4
+print('*\t\t  *********  ********  **\t\t   *\n*\t\t  *\t\t\t *\t\t   **\t\t  *  *')
+print('*\t\t  *\t\t\t *\t\t   **\t\t *\t  *\n*\t\t  *\t\t\t *\t\t   **\t\t*\t   *')
+print('*\t\t  *********  ********  **\t   **********\n*\t\t  *\t\t\t\t\t*  **\t  *\t\t\t *')
+print('*\t\t  *\t\t\t\t\t*  **\t *\t\t\t  *\n*\t\t  *\t\t\t\t\t*  **\t*\t\t\t   *')
+print('********  *********  ********  **  *\t\t\t\t*')
+print('\n')
+
+#task 5
+print(''' 
+Escape sequences
+\\a\t\tBell (alert)
+\\b\t\tBackspace
+\\n\t\tNew line
+\\t\t\tHorizontal tab
+\\\ \t\tBackslash \ 
+\\"\t\tDouble quotation mark "
+\\'\t\tSingle quotation mark ' ''')
