ca3f0b5cfcdedeac29f0dddda23a26ee8f5077fe
olesiasvetlova <lesiapaska91@gmail.com>
2023-02-07 17:42:44 +0200

    OOP_№1

--- oop.py
+++ oop.py
@@ -3,6 +3,16 @@ import csv
 
 
 class Employee:
+    """
+     Класс Employee содержащий следуюущие свойства:
+    'firstname'
+    'lastname'
+    'age'
+    'e-mail'
+    'skills'
+    'people_lang'
+    'coding_lang'
+    """
     def __init__(self, firstname, lastname, age, email, skills, people_lang, coding_lang):
         self.firstname = firstname
         self.lastname = lastname
@@ -12,14 +22,15 @@ class Employee:
         self.people_lang = people_lang
         self.coding_lang = coding_lang
 
-    def display_info(self):
+    def display_info(self):  # метод который выводит информацию о сотруднике на консоль с атрибутами
+        # self.firstname,self.lastname,self.age
         print(f"Firstname: {self.firstname}\nLastname: {self.lastname}\nAge: {self.age}")
 
-    def write_json(self):
+    def write_json(self):  # метод записи данных сотрудника в файл JSON
         with open('dict.json', 'w') as filename:
             json.dump(vars(self), filename, indent=4)
 
-    def write_csv(self):
+    def write_csv(self):  # метод записи данных сотрудника в файл СSV
         with open('dict.csv', 'w', newline='') as csv_file:
             writer = csv.writer(csv_file, delimiter=';')
             for key, value in vars(self).items():
